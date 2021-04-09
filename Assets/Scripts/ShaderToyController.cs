222e19f6d3ae74ad0f49abb5ff54e4da6ee95c2c
Gaoliyang96 <648653259@qq.com>
2021-04-09 13:17:07 +0800

    ShderToy在unity片元着色器中的基本逻辑

---
+++ Assets/Scripts/ShaderToyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderToyController : MonoBehaviour
+{
+    public Shader shaderToy;
+    private Material shaderToyMaterial = null;
+
+    public Material Material
+    {
+        get
+        {
+            shaderToyMaterial = GetMat(shaderToy, shaderToyMaterial);
+            return shaderToyMaterial;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, Material);
+    }
+
+    Material GetMat(Shader shader, Material material)
+    {
+        if(shader == null)
+        {
+            return null;
+        }
+
+        if (!shader.isSupported)
+        {
+            return null;
+        }
+        else
+        {
+            material = new Material(shader)
+            {
+                hideFlags = HideFlags.DontSave
+            };
+            return material;
+        }
+    }
+}
